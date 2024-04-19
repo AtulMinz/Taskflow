b140de294a8384db811b7c0840b6536bd592407b
AtulMinz <atulminz.jsr@gmail.com>
2024-04-20 05:29:31 +0530

    Added flow configuration

---
+++ src/config.js
@@ -0,0 +1,8 @@
+import { config } from "@onflow/fcl";
+
+config({
+  "app.detail.title": "Beflow",
+  "app.detail.icon": "https://cdn.worldvectorlogo.com/logos/next-js.svg",
+  "accessNode.api": "https://rest-testnet.onflow.org",
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+});
