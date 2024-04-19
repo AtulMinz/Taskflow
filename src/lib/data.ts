2334851db15155b7ed3ec1d7d9a4fdcf1f7cea49
AtulMinz <atulminz.jsr@gmail.com>
2024-04-20 04:25:42 +0530

    Added data file

---
+++ src/lib/data.ts
@@ -0,0 +1,34 @@
+export const services = [
+  {
+    name: "Cleaning",
+    description: "Clean the whole house",
+    image: "cleaning.svg",
+    price: "10",
+    workerName: "Atul",
+    hours: "3",
+  },
+  {
+    name: "Cleaning",
+    description: "Clean the whole house",
+    image: "",
+    price: "10",
+    workerName: "Atul",
+    hours: "3",
+  },
+  {
+    name: "Cleaning",
+    description: "Clean the whole house",
+    image: "",
+    price: "10",
+    workerName: "Atul",
+    hours: "3",
+  },
+  {
+    name: "Cleaning",
+    description: "Clean the whole house",
+    image: "",
+    price: "10",
+    workerName: "Atul",
+    hours: "3",
+  },
+];
