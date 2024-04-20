4ca6bfcdc74ead84042b8e6f0846d1dcb3618a20
AtulMinz <atulminz.jsr@gmail.com>
2024-04-21 02:19:14 +0530

    Fixed linting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,3 @@
-//@ts-nocheck
 "use client";
 
 import { useRouter } from "next/navigation";
@@ -10,19 +9,19 @@ export default function About() {
       <div className="w-4/5">
         <div className="h-[80vh] flex items-center justify-center ">
           <p className="text-xl text-neutral-600 font-serif">
-            We at &apos;
+            We at{" "}
             <span
               className="underline decoration-indigo-500 font-black hover:cursor-pointer"
               onClick={() => router.push("/")}
             >
               Taskflow
-            </span>
-            &apos; understand the hustle of daily life. Between work, family,
-            and errands, it's easy to let things like house cleaning and pet
-            care fall by the wayside. That's where we come in. We are a platform
-            that connects you with reliable service providers for everyday
-            needs, all with the convenience of a few clicks. But what sets us
-            apart is our innovative payment system: Flow.
+            </span>{" "}
+            understand the hustle of daily life. Between work, family, and
+            errands, it&apos;s easy to let things like house cleaning and pet
+            care fall by the wayside. That&apos;s where we come in. We are a
+            platform that connects you with reliable service providers for
+            everyday needs, all with the convenience of a few clicks. But what
+            sets us apart is our innovative payment system.
           </p>
         </div>
       </div>
