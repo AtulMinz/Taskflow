be5c87a28d5cfd5c989de196b51f1d61f1995a80
AtulMinz <atulminz.jsr@gmail.com>
2024-04-20 14:18:49 +0530

    Added images

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,8 @@ export const services = [
   {
     name: "Cleaning",
     description: "Clean the whole house",
-    image: "cleaning.svg",
+    image:
+      "https://static.vecteezy.com/system/resources/previews/005/571/603/original/cleaning-service-background-with-hand-drawn-black-and-white-elements-and-lettering-illustration-in-doodle-style-banner-template-vector.jpg",
     price: 10,
     workerName: "Atul",
     hours: "3",
