feede1ec6180a694c173e3a14ac5ca81f861d513
AtulMinz <atulminz.jsr@gmail.com>
2024-04-20 18:27:12 +0530

    configuration update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import { config } from "@onflow/fcl";
 
 config({
-  "app.detail.title": "Beflow",
+  "app.detail.title": "Taskflow",
   "app.detail.icon": "https://cdn.worldvectorlogo.com/logos/next-js.svg",
   "accessNode.api": "https://rest-testnet.onflow.org",
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
