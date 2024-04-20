45a1bf1529044bd314aa53b17fb05d1bdf10b910
AtulMinz <atulminz.jsr@gmail.com>
2024-04-21 01:41:43 +0530

    Updated dataset

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,8 @@
 export const services = [
   {
-    name: "Cleaning",
-    description: "Clean the whole house",
+    name: "Cleaning Service",
+    description:
+      "Sparkling Homes, Stress-Free Life: Reclaim your weekends and unwind in a spotless space. Our cleaning services offer customizable options to fit your needs, from a quick refresh to a deep clean. Breathe easy knowing your home is sparkling with eco-friendly products.",
     image:
       "https://static.vecteezy.com/system/resources/previews/005/571/603/original/cleaning-service-background-with-hand-drawn-black-and-white-elements-and-lettering-illustration-in-doodle-style-banner-template-vector.jpg",
     price: 49.9,
@@ -10,7 +11,8 @@ export const services = [
   },
   {
     name: "Gardening Service",
-    description: "Clean the whole house",
+    description:
+      "Blooming Gardens, Year-Round: Transform your outdoor space into a flourishing oasis. Our experienced gardeners offer a variety of services, from lawn care and seasonal planting to garden design and maintenance. Relax and enjoy the beauty of a thriving garden.",
     image:
       "https://img.freepik.com/free-vector/home-gardening-logo_17005-1524.jpg",
     price: 99.9,
@@ -19,7 +21,8 @@ export const services = [
   },
   {
     name: "Pet Care",
-    description: "Clean the whole house",
+    description:
+      "Happy Pets, Happy You: Give your furry friend the love and attention they deserve. Our reliable pet care providers offer services like dog walking, pet sitting, and home visits. Enjoy peace of mind knowing your pet is well-cared for while you're away.",
     image:
       "https://i.pinimg.com/originals/46/79/8c/46798c3b6cc788748a78d5660b619ada.jpg",
     price: 149.9,
