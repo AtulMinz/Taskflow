3a68cce42bb3bc8b22804c80e2a9e2e0f3bf2508
AtulMinz <atulminz.jsr@gmail.com>
2024-04-23 15:44:48 +0530

    Updated dataset from Number to String

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,7 +5,7 @@ export const services = [
       "Sparkling Homes, Stress-Free Life: Reclaim your weekends and unwind in a spotless space. Our cleaning services offer customizable options to fit your needs, from a quick refresh to a deep clean. Breathe easy knowing your home is sparkling with eco-friendly products.",
     image:
       "https://static.vecteezy.com/system/resources/previews/005/571/603/original/cleaning-service-background-with-hand-drawn-black-and-white-elements-and-lettering-illustration-in-doodle-style-banner-template-vector.jpg",
-    price: 49.9,
+    price: "49.9",
     workerName: "Victor & Liam",
     time: "12 hours/week",
   },
@@ -15,7 +15,7 @@ export const services = [
       "Blooming Gardens, Year-Round: Transform your outdoor space into a flourishing oasis. Our experienced gardeners offer a variety of services, from lawn care and seasonal planting to garden design and maintenance. Relax and enjoy the beauty of a thriving garden.",
     image:
       "https://img.freepik.com/free-vector/home-gardening-logo_17005-1524.jpg",
-    price: 99.9,
+    price: "99.9",
     workerName: "Alex & John",
     time: "30 hours/week",
   },
@@ -25,7 +25,7 @@ export const services = [
       "Happy Pets, Happy You: Give your furry friend the love and attention they deserve. Our reliable pet care providers offer services like dog walking, pet sitting, and home visits. Enjoy peace of mind knowing your pet is well-cared for while you're away.",
     image:
       "https://i.pinimg.com/originals/46/79/8c/46798c3b6cc788748a78d5660b619ada.jpg",
-    price: 149.9,
+    price: "149.9",
     workerName: "Maria",
     time: "24 hours/week",
   },
